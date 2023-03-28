d86f061f2512d230bf6a99b596cec8fe8db8ee4f
张杰 <zhangjie@fanxiapp.com>
2023-03-28 17:25:14 +0800

    feat:test

---
+++ src/First/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type DemoProps = {
+  /**
+   * @description 需要展示的文字
+   * @default undefined
+   */
+  title: string;
+};
+
+// 这是一个demo
+export default ({ title }: DemoProps) => {
+  const a = 1;
+  const fun= () =><></>;
+  return <h1>{title}</h1>;
+};
