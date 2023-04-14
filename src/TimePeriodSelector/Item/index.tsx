4e274bcabd0f26ca746795eef6f2313a8db41ca2
steady_man <486617171@qq.com>
2023-04-14 14:23:06 +0800

    feat:完善时间周期选择组件

--- src/TimePeriodSelector/Item/index.tsx
+++ src/TimePeriodSelector/Item/index.tsx
@@ -5,18 +5,20 @@ import './index.css';
 
 type Props = {
   value: number;
-  selected: boolean;
   selectedColor?: TimePeriodSelectorProps['selectedColor']; // 自定义选中颜色
   isSelected?: boolean;
   selectableRef?: LegacyRef<HTMLDivElement>;
 };
 
 const Item = (props: Props) => {
-  const { selectableRef, selected, selectedColor } = props;
+  const { selectableRef, selectedColor, isSelected } = props;
+
   return (
     <div
       className={'time-period-selector-wrapper-list-item'}
-      style={selected ? { background: selectedColor || '#03a9f4' } : undefined}
+      style={
+        isSelected ? { background: selectedColor || '#03a9f4' } : undefined
+      }
       ref={selectableRef}
     />
   );
